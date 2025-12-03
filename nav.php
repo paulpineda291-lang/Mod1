db49012a0338da8141b71b2713232f3204df4905
paulpineda291-lang <paulpineda291@gmail.com>
2025-12-03 21:42:48 +0800

    Delete nav.php

--- nav.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <a href="OperatorsFunction.php">Operators</a> |
-    <a href="ArrayFunction.php">Arrays</a> |
-    <a href="ShorthandFunction.php">Shorthand</a> |
-    <a href="TypejugglingConditionals.php">Type Juggling</a>
-</nav>
-<hr>
